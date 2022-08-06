d676377d8303b600627c0f1d3316db5153461a16
Rajath-Acharya <rajath365@gmail.com>
2022-08-06 15:30:57 +0530

    server init

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import express, {Response}  from 'express';
+
+const server = express();
+
+server.get('/', (_, res:Response) => {
+  res.send('Hello World!')
+})
+
+server.listen(4000, () => {
+  console.log('listening');
+})
